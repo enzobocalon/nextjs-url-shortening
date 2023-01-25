f185a98d75045d9bdfff7591c87123ea5fdf2145
Enzo Bocalon <enzo.bocalon@hotmail.com>
2023-01-25 17:09:33 -0300

    feat: fixed missing Head component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,10 +8,21 @@ import Shortening from '@/components/Shortening';
 import GetStarted from '@/components/GetStarted';
 import Footer from '@/components/Footer';
 import LinksProvider from '@/contexts/LinkContext';
+import Head from 'next/head';
 
 export default function Home() {
 	return (
 		<ThemeProvider theme={theme}>
+			<Head>
+				<title>URL Shortening</title>
+				<meta name='description' content='URL Shortening' />
+				<meta property='og:title' content='URL Shortening' />
+				<meta
+					property='og:description'
+					content='URL Shortening from Front End Mentor'
+				/>
+				<meta property='og:type' content='website' />
+			</Head>
 			<LinksProvider>
 				<Header />
 				<Hero />
