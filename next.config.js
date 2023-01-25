b64b67fc09debf8af6c0b7c0d414435fc5cb934b
Enzo Bocalon <enzo.bocalon@hotmail.com>
2023-01-25 17:03:37 -0300

    fixed eslint for production

--- next.config.js
+++ next.config.js
@@ -1,10 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+	reactStrictMode: true,
+	swcMinify: true,
 	compiler: {
 		styledComponents: true
+	},
+	eslint: {
+		ignoreDuringBuilds: true
 	}
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
